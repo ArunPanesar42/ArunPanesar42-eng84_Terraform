f7f72e5909230d2517b8c5c6bef7b5a4a24a4f86
ArunPanesar42 <ArunSPanesar@gmail.com>
2021-05-10 17:08:38 +0100

    Udated README

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # Terraform
 ## Terraform most used commands 
+- terraform init
+- terraform plan
+- terraform apply
+- terraform destroy
 ### Terraform to launch EC2 with VPC, subnets, SG services of AWS
+```
+provider "aws"{
+   region = "eu-west-1"
+}resource "aws_instance" "app_instance"{
+	ami = "ami-0f852bb43834ab266"
+
+	instance_type = "t2.micro"
+
+	associate_public_ip_address = true
+
+	tags = {
+		Name = "eng84_arun_terraform_node_app"
+	}
+}
+
+```
 ### Terraform installation and settings path in the env variable 
 #### Sercuring AWS keys with Terraform 
+
 
 
 
--- main.tf
+++ main.tf
@@ -25,13 +25,30 @@ resource "aws_instance" "app_instance"{
 	# lets add instance type we would like to launch 
 	instance_type = "t2.micro"
 
+	# This would be used in old versions 
+	#AWS_ACCESS_KEY:"" 
+	#AWS_SECRET_KEY:""
+
 	# Need to associate a public IP for our app
 	associate_public_ip_address = true
 	# Tags are used to give a name to our instance
 	tags = {
 		Name = "eng84_arun_terraform_node_app"
 	}
+
+	key_name = "eng84devops"
 }
+
+# Resource for vpc
+resource "aws_default_vpc" "default" {
+	#cidr_block = "10.0.0.0/16"
+	#instance_tenancy = "default"
+
+	tags = {
+    	Name = "eng84_arun_terraform_VPC"
+  	}
+}
+
 # Resouce block of code ends here 
 
 # terraform init
